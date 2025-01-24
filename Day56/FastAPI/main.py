17bac1ae7a49ae4f6f3182bc8a726f7e4999474e
gcbeltrao <gbcarvalhof@gmail.com>
2025-01-24 15:56:14 -0300

    feat: Add Day56 FastAPI project files

---
+++ Day56/FastAPI/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.requests import Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
